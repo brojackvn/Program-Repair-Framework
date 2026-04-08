fc5692e6bac41e7c2b9a1756eeb747ff299abffd
brojackvn <anhho.work@gmail.com>
2026-04-08 10:36:01 +1000

    Added the scripts to run experiments

---
+++ scripts/ablation-study/pyregression/ablation_pyregression_conversational_apr_cc.sh
@@ -0,0 +1,34 @@
+cd /external_disk/coding_space/ChatRepairRegression
+
+# PANDAS
+# BUG_ID=(1 2 3 4 5 6 8 9 11 12 14 16 17 18 19 20 21 22)
+# DJANGO
+# BUG_ID=(15 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 43 44 45 46 48 49 50)
+
+for i in "${BUG_ID[@]}";  
+do
+    echo "********************************************************************"
+    echo "Processing BUG_ID=$i"
+
+    # Add selected Java
+    export PATH=$JAVA_HOME/bin:$PATH
+    
+    PYTHONPATH=/external_disk/coding_space/ChatRepairRegression \
+    python3 -m src.main conversational-apr-with-bic \
+        --additional-information "cc" \
+        --dataset pyregression \
+        --data-id "$i" \
+        --input-dir /external_disk/coding_space/ChatRepairRegression/experiments/pyregression-bug-metadata.json \
+        --mapping-dir /external_disk/coding_space/ChatRepairRegression/experiments/mapping-pyregression.txt \
+        --output-dir /external_disk/coding_space/ChatRepairRegression/experiments/ablation-output \
+        --env-dir /external_disk/coding_space/ChatRepairRegression/experiments/environments/pyregression \
+        --time-limit 720 \
+        --attempts 10 \
+        --iterations 5 \
+        --model-name "gpt-4o" \
+        --temperature 1 \
+        --top-p 0.95 \
+        --early-stop 
+
+    echo "********************************************************************"
+done
--- scripts/ablation-study/regminer4apr/ablation_regminer4apr_conversational_apr_cc.sh
+++ scripts/ablation-study/regminer4apr/ablation_regminer4apr_conversational_apr_cc.sh
@@ -1,15 +1,14 @@
 cd /external_disk/coding_space/ChatRepairRegression
 
-# BUG_ID=(1 2 3 5 10 12 13 17 21 22 24 25 28 30 31 33 34 35 36 37 38 39 40 41 42 43 44 46 47 48 52 54 55 56 63 65 66 72 73 74 75 76 77 81 82 84 95)
-# BUG_ID=(4 11 15 16 18 26 50 59 62 64 69 70 71 78 83 85 94 99)
-# BUG_ID=(100 101 103 104 105 106 107 108 109 111 112 113 114 115 117 118 120 121 122 123 124)
-BUG_ID=(125 126 127 128 129 130 131 134 135 136 137 138 139 140 143 144 145 146 147 148 150)
-
-# ERROR
-# BUG_ID=(102)
+# BUG_ID=(1 2 3 5 10 12 13 17 21 22 25 28 30 31 33 34 35 36 37 38 39 40 41 24)
+# BUG_ID=(42 43 44 46 47 48 52 54 55 56 63 65 66 72 74 75 76 77 81 82 84 95 73)
+# BUG_ID=(4 11 15 16 18 26 50 59 62 64 69 70 71 78 83 85 94 99 4)
+# BUG_ID=(100 102 101 103 104 105 106 107 108 109 111 112 113 114 115 117 118 120 121 122 123 124)
+# BUG_ID=(125 126 127 128 129 130 131 134 135 136 137 138 139 140 143 144 145 146 147 148 150)
 
 # IDs that require Java 11
-JAVA11_IDS=(100 110 144 145 147)
+JAVA11_IDS=(100 110 144 145)
+
 
 for i in "${BUG_ID[@]}";  
 do
@@ -29,18 +28,20 @@ do
 
     # Add selected Java
     export PATH=$JAVA_HOME/bin:$PATH
-    python3 -m src.main conversational-apr \
+    PYTHONPATH=/external_disk/coding_space/ChatRepairRegression \
+    python3 -m src.main conversational-apr-with-bic \
+        --additional-information "cc" \
         --dataset regminer4apr \
         --data-id "$i" \
         --input-dir /external_disk/coding_space/ChatRepairRegression/experiments/regminer4apr-bug-metadata.json \
-        --mapping-dir /external_disk/coding_space/ChatRepairRegression/experiments/mapping.txt \
-        --output-dir /external_disk/coding_space/ChatRepairRegression/experiments/output_test \
-        --tmp-dir /external_disk/coding_space/ChatRepairRegression/experiments/tmp-conversation \
+        --mapping-dir /external_disk/coding_space/ChatRepairRegression/experiments/mapping-regminer4apr.txt \
+        --output-dir /external_disk/coding_space/ChatRepairRegression/experiments/ablation-output \
+        --tmp-dir /external_disk/coding_space/ChatRepairRegression/experiments/tmp-conversation-bic \
         --env-dir /external_disk/coding_space/ChatRepairRegression/experiments/environments/regminer4apr \
         --time-limit 1800 \
         --attempts 10 \
         --iterations 5 \
-        --model-name "gpt-3.5-turbo" \
+        --model-name "gpt-4o" \
         --temperature 1 \
         --top-p 0.95 \
         --early-stop 
