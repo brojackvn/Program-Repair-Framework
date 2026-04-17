cfc5f294bf4cc3bf58e85a8d8e779482889e976c
brojackvn <anhho.work@gmail.com>
2026-04-17 10:14:07 +1000

    Edited the scripts

--- scripts/ablation-study/pyregression/ablation_pyregression_conversational_apr_cm.sh
+++ scripts/ablation-study/pyregression/ablation_pyregression_conversational_apr_cm.sh
@@ -5,8 +5,6 @@ cd /external_disk/coding_space/ChatRepairRegression
 # DJANGO
 # BUG_ID=(15 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 43 44 45 46 48 49 50)
 
-BUG_ID=(14)
-
 for i in "${BUG_ID[@]}";  
 do
     echo "********************************************************************"
